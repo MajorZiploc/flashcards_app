54c8617d86c3afcc0333d2f02aa0b738cc1eb6d1
Manyu Lakhotia <lakemonyew@gmail.com>
2024-07-27 10:33:46 -0500

    Add flip card dep and basic example

--- src/modules/flashcards/StudySession.js
+++ src/modules/flashcards/StudySession.js
@@ -1,6 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
 import { Button } from '../../components';
+import FlipCard from 'react-native-flip-card'
+
+export const Gray300 = '#E0E0E0'
+export const Gray100 = '#F5F5F5'
+export const Gray50 = '#FAFAFA'
+export const White = '#FFFFFF'
+export const Red = '#CE1126'
+export const Green = '#007A3D'
+
+export const { width, height } = Dimensions.get('window');
 
 function StudySession({ loadCards, loadCardsAsync, cards, isDefinitionFirst }) {
   const [studyCards, setStudyCards] = useState([]);
@@ -24,6 +34,19 @@ function StudySession({ loadCards, loadCardsAsync, cards, isDefinitionFirst }) {
 
   return (
     <View style={styles.container}>
+      <FlipCard
+        style={styles.flipCard}
+        perspective={1000}
+        flipHorizontal={true}
+        flipVertical={false}
+      >
+        <View style={styles.flipSide}>
+          <Text style={styles.face}>question</Text>
+        </View>
+        <View style={styles.flipSide}>
+          <Text style={styles.back}>answer</Text>
+        </View>
+      </FlipCard>
       <Text>Welcome to the Study Session!</Text>
       <Text size={20}>{`${isDefinitionFirst}`}</Text>
       {(studyCards ?? []).map(c => (
@@ -52,6 +75,38 @@ const styles = StyleSheet.create({
   button: {
     marginTop: 8,
     marginBottom: 8,
+  },
+  flipCard: {
+    backgroundColor: White,
+    alignItems: 'center',
+    shadowOpacity: 0.5,
+    shadowOffset: {
+      height: 5,
+      width: 5,
+    },
+    width: width - 20,
+    height: height - 200,
+    marginTop: 5,
+    marginLeft: 10,
+    marginRight: 10,
+    borderWidth: 0,
+  },
+  flipSide: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  face: {
+    fontSize: 25,
+    textAlign: 'center',
+    width: width - 40,
+    color: Red,
+  },
+  back: {
+    fontSize: 25,
+    textAlign: 'center',
+    width: width - 40,
+    color: Green,
   },
 });
 
