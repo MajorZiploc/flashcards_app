4b1ad5ab2b9daff4a68cac679d686987d9b14383
Manyu Lakhotia <lakemonyew@gmail.com>
2024-07-27 19:58:49 -0500

    -components and misc stuff

--- src/modules/navigation/tabNavigationData.js
+++ src/modules/navigation/tabNavigationData.js
@@ -1,19 +1,12 @@
 import FlashCardsHomeScreen from '../flashcards/FlashCardsHomeViewContainer';
-import ComponentsScreen from '../components/ComponentsViewContainer';
 
 const iconHome = require('../../../assets/images/tabbar/home.png');
-const iconComponents = require('../../../assets/images/tabbar/components.png');
 
 const tabNavigationData = [
   {
     name: 'Flash Cards',
     component: FlashCardsHomeScreen,
     icon: iconHome,
-  },
-  {
-    name: 'Components',
-    component: ComponentsScreen,
-    icon: iconComponents,
   },
 ];
 
