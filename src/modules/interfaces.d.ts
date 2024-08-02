06a8a5f2d4fe42c197e8d2805bcb791df2b46f0d
Manyu Lakhotia <lakemonyew@gmail.com>
2024-08-02 15:13:51 -0500

    ^types

--- src/modules/interfaces.d.ts
+++ src/modules/interfaces.d.ts
@@ -1,7 +1,18 @@
+export type useState<T> = [T, React.Dispatch<React.SetStateAction<T>>];
+
 export interface DBCard {
   id: any;
   term: string;
   definition: string;
+  deckId: any;
+}
+
+export interface StudyCard {
+  id: any;
+  term: string;
+  definition: string;
+  front: string;
+  back: string;
   deckId: any;
 }
 
