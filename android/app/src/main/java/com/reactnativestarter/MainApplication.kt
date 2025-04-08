916b20ba6be6bea99524099c5d22eef55eb545ca
Manyu Lakhotia <lakemonyew@gmail.com>
2025-04-08 17:47:30 -0500

    rename app

--- android/app/src/main/java/com/reactnativestarter/MainApplication.kt
+++ android/app/src/main/java/com/reactnativestarter/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.reactnativestarter
+package com.flashcardslink
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/ReactNativeStarterTests/FlashCardsLinkTests.m
+++ ios/ReactNativeStarterTests/FlashCardsLinkTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React!"
 
-@interface ReactNativeStarterTests : XCTestCase
+@interface FlashCardsLinkTests : XCTestCase
 
 @end
 
-@implementation ReactNativeStarterTests
+@implementation FlashCardsLinkTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
